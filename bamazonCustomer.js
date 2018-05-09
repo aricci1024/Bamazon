913dae687bc1b31222b3cbb6c7fa751c6ecc7ef6
aricci <alexander.p.ricci@gmail.com>
2018-05-09 16:57:24 -0700

    create function to show all items and their info and prompt user. Started .then to process user order.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,74 @@ var connection = mysql.createConnection({
     password: "",
     database: "Bamazon"
 });
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    queryAllItems();
+});
+
+function queryAllItems() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log("See store products below: ")
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " | Product Name: " + res[i].product_name + " | Department: " + res[i].department_name + " | Unit price: " + res[i].price + " | Quantity in stock: " + res[i].stock_quantity);
+        }
+        console.log("-----------------------------------");
+        userPurchase ();
+    })
+};
+
+function userPurchase () {
+    inquirer.prompt([
+        {
+            name: "item_id",
+            type: "input",
+            message: "Enter the ID of the item you'd like to purchase",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "quantRequest",
+            type: "input",
+            message: "How many units would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                return true;
+                }
+                return false;
+            }
+        }
+    ])
+    .then(function(answer) {
+        console.log("Item selected: " + input.item_id + "Quantity selected: " + input.quantRequest)
+        var query = 'SELECT * FROM Products WHERE itemID=' + answer.item_id;
+        connection.query(query, function(err, res) {
+            if (answer.Quantity <= res) {
+                for (var i = 0; i < res.length; i++) {
+                    console.log("Your order of "+ res[i].stock_quantity + " " + res[i].product_name + " is now being processed.");
+                    console.log("Your credit card will be charged " + res[i].stock_quantity*res[i].price)
+                    console.log("Thank you for your order!")
+                    connection.query("UPDATE products SET ? WHERE ?", [{
+                        StockQuantity: res[i].stock_quantity - quantRequest
+                    }, 
+                    {
+                        id: res[chosenId].id
+                    }], 
+                    function(err, res) {
+                        queryAllItems();
+                    });
+    
+                }
+            } else {
+                console.log("Insufficient quantity!");
+            }
+            queryAllItems();
+        })
+    });
+}
